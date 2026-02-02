ed463d6c51d26d92422b2c746dea4fb31592def1
hudhaifagburi2 <hudhaifagburi@gmail.com>
2026-02-02 15:25:41 +0300

    phase 4 BBS use loop: frontend/tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./admin/**/*.html",
+    "./client/**/*.html",
+    "./js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
